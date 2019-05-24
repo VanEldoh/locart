7111943d80d38a89dbabd51b84315a2af7ea272a
MARIEL BA <marielba82@yahoo.fr>
2019-05-24 15:32:08 +0200

    Booking controllers and views created" "

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/update'
-  get 'artworks/index'
-  get 'artworks/new'
-  get 'artworks/create'
-  get 'artworks/show'
   devise_for :users
   root to: 'pages#home'
   resources :artworks, only: [:index, :new, :create, :show] do
-    resources :bookings, only: [:new, :create, :index, :update]
+    resources :bookings, only: [:new, :create, :index, :edit, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
