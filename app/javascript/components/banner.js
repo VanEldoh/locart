69a1588328169c82fdec69ef3edd371edca5e1fc
Patricia Falandysz <pfalandysz@yahoo.com>
2019-05-28 18:35:01 +0200

    new home

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#typed', {
-    strings: ["First ^1000 sentence.", "Second sentence."],
-    typeSpeed: 50,
+    strings: ["Check out our latest highlights !"],
+    typeSpeed: 120,
     loop: true
   });
 }
