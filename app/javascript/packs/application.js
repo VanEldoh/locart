13560a0bce272a3b8b71f95376efa380479b16d5
Patricia Falandysz <pfalandysz@yahoo.com>
2019-05-28 20:46:35 +0200

    algolia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initAutocomplete } from '../plugins/init_autocomplete';
+initAutocomplete();
+
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
