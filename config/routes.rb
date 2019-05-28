ef58783b97a7ed324ae90e96a053b9b902e42c27
MARIEL BA <marielba82@yahoo.fr>
2019-05-28 18:53:55 +0200

    update booking page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'artworks#index'
+
+
   resources :artworks, only: [:index, :new, :create, :show] do
     collection do
       get 'all'
     end
     resources :bookings, only: [:new, :create, :index, :edit, :update]
+  end
+
+  resources :bookings, only: [] do
+    post 'booking/status_change', to: 'bookings#status_change'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
