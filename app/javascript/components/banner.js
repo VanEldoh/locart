d78991ebb43b845149c2b8a0271235bf27d126fe
MARIEL BA <marielba82@yahoo.fr>
2019-05-29 12:42:23 +0200

    map zoom disabled

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,15 @@
 import Typed from 'typed.js';
 
+const typed = document.getElementById('typed');
+
 const loadDynamicBannerText = () => {
-  new Typed('#typed', {
-    strings: ["Paintings | Drawings | Prints | Photography | Design"],
-    typeSpeed: 120,
-    loop: false
-  });
+  if (typed) {
+    new Typed('#typed', {
+      strings: ["Paintings | Drawings | Prints | Photography | Design"],
+      typeSpeed: 120,
+      loop: false
+    });
+  }
 }
 
 export { loadDynamicBannerText };
